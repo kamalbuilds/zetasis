8ed0de07108a7a68287188d43cf06e6418493e89
0xcarhartt <0xcarhartt@gmail.com>
2023-06-13 20:19:19 -0500

    add static data

---
+++ my-app/data/static.ts
@@ -0,0 +1,265 @@
+export const tokens = [
+  {
+    image: "/01.jpg",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/02.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/03.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/04.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/05.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/06.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/07.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/08.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/09.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/010.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/011.jpg",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/012.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/01.jpg",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/02.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/03.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/04.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/05.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/06.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/07.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/08.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/09.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/010.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/011.jpg",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/012.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+];
+
+export const azukiTokens = [
+  {
+    image: "/azuki/1.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/azuki/2.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 2,
+  },
+  {
+    image: "/azuki/3.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 3,
+  },
+  {
+    image: "/azuki/4.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/azuki/5.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/azuki/6.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/azuki/7.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/azuki/8.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/azuki/9.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/azuki/10.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/azuki/11.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/azuki/12.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/azuki/13.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/azuki/14.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/azuki/15.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+  {
+    image: "/azuki/16.png",
+    name: "Token Name",
+    owner: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    id: 1,
+  },
+];
+
+export const collections = {
+  "1": {
+    image: "/silicate.png",
+    title: "Silicate Collection 1",
+    creator: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    description:
+      "Silicate is the creative revolution. Silicate is the creative revolution. Silicate is the creative revolution. Silicate is the creative revolution. Silicate is the creative revolution.",
+    tokens: tokens,
+  },
+  "2": {
+    image: "/azuki.png",
+    title: "AZUKI (Mantle Edition)",
+    creator: "0xB8E93eF7B186A090d15fe4fb3F76A0E1d643b86A",
+    description:
+      "Azuki starts with a collection of 10,000 avatars that give you membership access to The Garden: a corner of the internet where artists, builders, and web3 enthusiasts meet to create a decentralized future.",
+    tokens: azukiTokens,
+    mintable: true,
+  },
+};
